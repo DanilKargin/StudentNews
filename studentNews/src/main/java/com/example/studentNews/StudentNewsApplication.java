62aea3856ef74bf904fba98e586883b8afb008eb
Danil Kargin <danilkargin16@gmail.com>
2024-11-23 20:29:24 +0400

    Add server side project

---
+++ studentNews/src/main/java/com/example/studentNews/StudentNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentNewsApplication.class, args);
+	}
+
+}
